d7d8a3a54bc28fbc52f1621ee540a2d1957e1c62
Quique Fdez Guerra <quique@ckgrafico.com>
2014-06-19 01:39:23 +0200

    Init from local

---
+++ js/app.js
@@ -0,0 +1,74 @@
+(function(g, $) {
+
+	var person = 1;
+	var verb = 0;
+
+	// On change an input
+	$('.config__person').on('change', function() {
+		person = parseInt($(this).val(), 10);
+		processTenses();
+	});
+
+	$('.config__verb').on('change', function() {
+		verb = parseInt($(this).val(), 10);
+		processTenses();
+	});
+
+	$('.config__filters').on('submit', function(e) {
+		e.preventDefault();
+		var footableFilter = $('.footable').data('footable-filter');
+		footableFilter.filter($('.config__filter').val());
+	});
+
+	function configOptions() {
+	}
+
+	function processTenses() {
+		var _verb = g.verbs[verb];
+		var _person = g.persons[person];
+		var _complement = g.complements[verb];
+		var tenses = g.tenses;
+		var container = $('.footable tbody');
+		container.html('');
+
+		for (var i = 0; i < tenses.length; i++) {
+			var tense = tenses[i];
+			var tr = $('<tr/>');
+			tr.append($('<td/>').addClass('footable-first-column').append($('<span/>').addClass('footable-toggle')).text(tense.name));
+			tr.append($('<td/>').text(
+
+				_person +
+				get(tense.afterPron, person) + ' ' +
+				_verb[tense.verb] +
+				get(tense.afterVerb, person) + ' ' +
+				_complement
+
+			));
+			container.append(tr);
+		}
+
+		$('.footable').footable();
+	}
+
+	// because object in JS...
+	function get(a, b) {
+		if (a) {
+			return a[b] || '';
+		}
+		return '';
+	}
+
+	processTenses();
+})(this, jQuery);
+
+/*
+container.append('
+				<tr>
+					<td class="footable-first-column"><span class="footable-toggle"></span>Present Simple</td>
+					<td>positive</td>
+					<td>negative</td>
+					<td>question</td>
+					<td class="comment">comment</td>
+				</tr>
+			');
+ */
---
+++ js/data.js
@@ -0,0 +1,110 @@
+(function(g) {
+
+	g.verbs = [
+		// regular
+		['play', 'played', 'played'],
+		// irregular
+		['go', 'went', 'gone'],
+		// to be
+		[]
+	];
+
+	g.persons = [
+		'I',
+		'You',
+		'He'
+	];
+
+	g.complements = [
+		'football',
+		'to school',
+		''
+	];
+
+	var tenses = [
+		{
+			name: 'Present Simple',
+			verb: 0,
+			afterVerb: ['', '', 's']
+		},
+
+		{
+			name: 'Past Simple',
+			verb: 1
+		},
+
+		{
+			name: 'Future Simple',
+			verb: 0,
+			afterPron: [' will', ' will', ' will'],
+		},
+
+		{
+			name: 'Future Simple \'going to\'',
+			verb: 0,
+			afterPron: [' am going to', ' are going to', ' is going to'],
+		},
+
+		{
+			name: 'Present Continuous',
+			verb: 0,
+			afterPron: [' am', ' are', ' is'],
+			afterVerb: ['ing', 'ing', 'ing']
+		},
+
+		{
+			name: 'Past Continuous',
+			verb: 0,
+			afterPron: [' were', ' were', ' was'],
+			afterVerb: ['ing', 'ing', 'ing']
+		},
+
+		{
+			name: 'Future Continuous',
+			verb: 0,
+			afterPron: [' will be', ' will be', ' will be'],
+			afterVerb: ['ing', 'ing', 'ing']
+		},
+
+		{
+			name: 'Present Perfect',
+			verb: 2,
+			afterPron: [' have', ' have', ' has']
+		},
+
+		{
+			name: 'Past Perfect',
+			verb: 2,
+			afterPron: [' had', ' had', ' had']
+		},
+
+		{
+			name: 'Future Perfect',
+			verb: 2,
+			afterPron: [' will have', ' will have', ' will has']
+		},
+
+		{
+			name: 'Present Perfect Continuous',
+			verb: 0,
+			afterPron: [' have been', ' have been', ' has been'],
+			afterVerb: ['ing', 'ing', 'ing']
+		},
+
+		{
+			name: 'Past Perfect Continuous',
+			verb: 0,
+			afterPron: [' had been', ' had been', ' had been'],
+			afterVerb: ['ing', 'ing', 'ing']
+		},
+
+		{
+			name: 'Future Perfect Continuous',
+			verb: 0,
+			afterPron: [' will have been', ' will have been', ' will has been'],
+			afterVerb: ['ing', 'ing', 'ing']
+		}
+	];
+
+	g.tenses = tenses;
+})(this);
